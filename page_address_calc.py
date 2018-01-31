867f16174be6728da7971e99b3ade9ddbc708797
Thomas Kercheval <spacekattpoispin@gmail.com>
2018-01-31 15:27:15 -0800

    Update `page_address_calc` docs

--- page_address_calc.py
+++ page_address_calc.py
@@ -1,7 +1,22 @@
-        # TODO: complete documentation
+'''
+Defines an page address calculator to be used to verify homework questions
+in my hardware class.
+'''
         # TODO: add to discord bot
-class AddressParser:
+        # TODO: add testing framework
+class AddressParser(object):
     '''
+    Calculates the addresses for the beginning and end of pages given
+    a specific memory chip. Chips are defined by the number of page, offset,
+    and byte-selector bits in its address space. Pages that cannot
+    exist within the chip will not have valid addresses, thus this
+    parser will not generate them (rather return an error message).
+
+    To get the start and end addresses for page number 7 of a chip
+    with 3 paging bits, 4 offset bits, and 5 byte-selector bits, we
+    would do the following:
+    >>> addy = AddressParser(3, 4, 5)
+    >>> print(addy.get_start_end(7)
     '''
     # The number of page bits
     page_bits = 0
@@ -15,7 +30,9 @@ class AddressParser:
     hex_length = 0
 
     def __init__(self, page, offset, byte_sel):
-        '''Initializes the data members of our object'''
+        '''
+        Initializes the data members of our object, obviously
+        '''
         self.page_bits = page
         self.offs_bits = offset
         self.byte_bits = byte_sel
@@ -24,43 +41,63 @@ class AddressParser:
 
     def to_hex(self, bin_num):
         '''
+        From a binary bumber, `bin_num`, convert it into its hexadecimal
+        representation (with uppercase letters). Also pads the hex number
+        with zeros in order to make it into an absolute address.
         '''
         value = hex(int(bin_num, 2))[2:].upper()
         value = '0x' + value.zfill(self.hex_length)
         return value
 
     def get_hex_length(self):
+        '''
+        Calculates the total length of a hexadecimal address for a
+        specific memory chip (e.g., a chip with 17 total bits will
+        have a hexidecimal address that is 5 characters long).
+        '''
         length = 1
         while length * 4 < self.total_bits:
             length += 1
         return length
 
-    def get_page(self, page_num):
-        '''
-        '''
-        page_bin = "{0:b}".format(page_num)
-        return page_bin
-
     def get_ending(self, is_start):
         '''
+        Returns the binary representation of the offset and byte-selector
+        bits. Since we are only concerned with the beginning or end
+        of a page's address, these bits will either all be zero or all
+        be one, if it is the start or end (respectively).
+
+        `is_start` indicates whether we are generating the start or end
+        of the page's address.
         '''
         remaining_bits = self.offs_bits + self.byte_bits
         if is_start:
+            # The start of page has all offset and byte-selector bits at zero
             return '0' * remaining_bits
         else:
+            # The end has all offset and b-s bits at one
             return '1' * remaining_bits
 
     def get_address(self, page_num, is_start):
         '''
+        Returns the absolute address of a specific page. Whether we
+        are at the start or end of the page is indicated by `is_start`.
         '''
-        page = self.get_page(page_num)
+        # Get the page number in its binary representation
+        page = "{0:b}".format(page_num)
+        # Get the offset and byte-selector bits
         offset_byte_sel = self.get_ending(is_start)
-
+        # The complete address, in binary
         address = page + offset_byte_sel
-        return self.to_hex(address).zfill(self.hex_length)
+        # Return the absolute hex address
+        return self.to_hex(address)
 
     def is_valid_page(self, page_num):
-        return (page_num < 2 ** self.page_bits and page_num >= 0)
+        '''
+        Valid page numbers are non-negative and less than 2 to the
+        power of the number of page bits we have.
+        '''
+        return page_num < 2 ** self.page_bits and page_num >= 0
 
     def get_start_end(self, page_num):
         '''
@@ -68,47 +105,59 @@ class AddressParser:
         '''
         if not self.is_valid_page(page_num):
             return "Invalid page number"
+        result = []
+        result[0] = self.get_address(page_num, True)
+        result[1] = self.get_address(page_num, False)
+        return result
+
+    def get_formatted_page_results(self, page_num):
+        '''
+        Formats results in a pretty fashion for display.
+        '''
+        if not self.is_valid_page(page_num):
+            return "Invalid page number"
+        page_addresses = self.get_start_end(page_num)
         result = "Chip summary:\n"
         result += "Page bits: {}, Offset bits: {}, Byte-select bits: {}\n"
         result = result.format(self.page_bits, self.offs_bits, self.byte_bits)
         result += "Addresses for page number {}:\n".format(page_num)
-        result += "Start: " + self.get_address(page_num, True) + '\n' 
-        result += "End:   " + self.get_address(page_num, False)
+        result += "Start: " + page_addresses[0] + '\n'
+        result += "End:   " + page_addresses[1]
         return result
 
-if __name__ == '__main__':
-    addre = AddressParser(4, 13, 0)
-    print('-------------------------------------------------')
-    print(addre.get_start_end(0))
-    print('-------------------------------------------------')
-    print(addre.get_start_end(1))
-    print('-------------------------------------------------')
-    print(addre.get_start_end(7))
-    print('-------------------------------------------------')
-    print(addre.get_start_end(15))
-    print('-------------------------------------------------')
+# if __name__ == '__main__':
+    # ADDRE = AddressParser(4, 13, 0)
+    # print('-------------------------------------------------')
+    # print(ADDRE.get_formatted_page_results(0))
+    # print('-------------------------------------------------')
+    # print(ADDRE.get_formatted_page_results(1))
+    # print('-------------------------------------------------')
+    # print(ADDRE.get_formatted_page_results(7))
+    # print('-------------------------------------------------')
+    # print(ADDRE.get_formatted_page_results(15))
+    # print('-------------------------------------------------')
 
-    addre = AddressParser(11, 17, 2)
-    print('-------------------------------------------------')
-    print(addre.get_start_end(0))
-    print('-------------------------------------------------')
-    print(addre.get_start_end(17))
-    print('-------------------------------------------------')
-    print(addre.get_start_end(381))
-    print('-------------------------------------------------')
-    print(addre.get_start_end(1024))
-    print('-------------------------------------------------')
-    print(addre.get_start_end(1094))
-    print('-------------------------------------------------')
-    addre = AddressParser(13, 17, 0)
-    print('-------------------------------------------------')
-    print(addre.get_start_end(0))
-    print('-------------------------------------------------')
-    print(addre.get_start_end(17))
-    print('-------------------------------------------------')
-    print(addre.get_start_end(381))
-    print('-------------------------------------------------')
-    print(addre.get_start_end(1024))
-    print('-------------------------------------------------')
-    print(addre.get_start_end(1094))
-    print('-------------------------------------------------')
+    # ADDRE = AddressParser(11, 17, 2)
+    # print('-------------------------------------------------')
+    # print(ADDRE.get_formatted_page_results(0))
+    # print('-------------------------------------------------')
+    # print(ADDRE.get_formatted_page_results(17))
+    # print('-------------------------------------------------')
+    # print(ADDRE.get_formatted_page_results(381))
+    # print('-------------------------------------------------')
+    # print(ADDRE.get_formatted_page_results(1024))
+    # print('-------------------------------------------------')
+    # print(ADDRE.get_formatted_page_results(1094))
+    # print('-------------------------------------------------')
+    # ADDRE = AddressParser(13, 17, 0)
+    # print('-------------------------------------------------')
+    # print(ADDRE.get_formatted_page_results(0))
+    # print('-------------------------------------------------')
+    # print(ADDRE.get_formatted_page_results(17))
+    # print('-------------------------------------------------')
+    # print(ADDRE.get_formatted_page_results(381))
+    # print('-------------------------------------------------')
+    # print(ADDRE.get_formatted_page_results(1024))
+    # print('-------------------------------------------------')
+    # print(ADDRE.get_formatted_page_results(1094))
+    # print('-------------------------------------------------')
